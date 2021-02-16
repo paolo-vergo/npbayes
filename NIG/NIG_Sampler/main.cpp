7d08635d12880ec0bfe602ec5feb52076aee1763
RiccardoMS <61756581+RiccardoMS@users.noreply.github.com>
2021-02-16 16:45:19 +0100

    Update main.cpp

--- NIG/NIG_Sampler/main.cpp
+++ NIG/NIG_Sampler/main.cpp
@@ -6,8 +6,8 @@
 using std::vector;
 int main() {
     //IMPORTED RDATA
-    vector<double> Data=read_data_csv(R"(C:\Users\aughi\Desktop\Hierarchical Bayesian Nonparametric models to smooth functional data\NIG_DataGeneration\Data.csv)");
-    vector<int>    Dims=read_dims_csv(R"(C:\Users\aughi\Desktop\Hierarchical Bayesian Nonparametric models to smooth functional data\NIG_DataGeneration\Dims.csv)");
+    vector<double> Data=read_data_csv(R"(YOUR PATH\NIG\NIG_DataGeneration\Data.csv)");
+    vector<int>    Dims=read_dims_csv(R"(YOUR PATH\NIG\NIG_DataGeneration\Dims.csv)");
 
     //Instantiate sampler object
     Sampler S(Data,Dims);
