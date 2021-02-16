e56940ee570217b0480766b6dc434fdad9eba181
RiccardoMS <61756581+RiccardoMS@users.noreply.github.com>
2021-02-16 16:55:33 +0100

    Update main.cpp

--- MultipleThrows/MultipleThrows_Sampler/main.cpp
+++ MultipleThrows/MultipleThrows_Sampler/main.cpp
@@ -6,9 +6,9 @@
 using std::vector;
 int main() {
     //Import data
-    vector<double> Data=read_data_csv(R"(C:\Users\aughi\Desktop\Hierarchical Bayesian Nonparametric models to smooth functional data\Shotput Dataset\Pre processing\Data\Data.csv)");
-    vector<int>    Dims=read_dims_csv(R"(C:\Users\aughi\Desktop\Hierarchical Bayesian Nonparametric models to smooth functional data\Shotput Dataset\Pre processing\Data\Dims.csv)");
-    vector<int>    Per_athlete_dims=read_dims_csv(R"(C:\Users\aughi\Desktop\Hierarchical Bayesian Nonparametric models to smooth functional data\Shotput Dataset\Pre processing\Data\per_athlete_dims.csv)");
+    vector<double> Data=read_data_csv(R"(YOUR PATH\MultipleThrows\MultipleThrows_DataGeneration\Data.csv)");
+    vector<int>    Dims=read_dims_csv(R"(YOUR PATH\MultipleThrows\MultipleThrows_DataGeneration\Dims.csv)");
+    vector<int>    Per_athlete_dims=read_dims_csv(R"(YOUR PATH\MultipleThrows\MultipleThrows_DataGeneration\per_athlete_dims.csv)");
 
     //Instantiate sampler object
     Sampler S(Data,Dims,Per_athlete_dims);
